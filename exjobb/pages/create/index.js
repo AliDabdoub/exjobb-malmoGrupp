095251612636b1249248fc89dbb9faabb09e4357
tildel <tildelienau@gmail.com>
2022-10-18 15:54:27 +0200

    Create

--- exjobb/pages/create/index.js
+++ exjobb/pages/create/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import EmblaCarousel from "./EmblaCarousel";
 
+let demo = React.createElement( 
+    "h1", { style: { color: "green" } }, "Welcome to GeeksforGeeks"
+)
+
+
+
+function create (){
+    return(
+        <div>
+            <h1>create outfits page</h1>
+        </div>
+    )
+}
 
 const SLIDE_COUNT = 5;
 const slides = Array.from(Array(SLIDE_COUNT).keys());
@@ -14,16 +27,10 @@ const App = () => (
   </main>
 );
 
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement); 
 
-function create (){
-    return(
-        <div>
-            <h1>create outfits page</h1>
-        </div>
-    )
-}
 
 export default create
 
