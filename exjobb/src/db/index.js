6b0d53af2e3b17a14f91b6fc72d11206f5d3bff3
tildel <tildelienau@gmail.com>
2022-10-19 14:16:20 +0200

    Help

--- exjobb/src/db/index.js
+++ exjobb/src/db/index.js
@@ -1,11 +1,13 @@
 import clothesJson from './clothes.json'
 
 export const media = clothesJson.clothes
+export const mediaTop = media.filter(typ => typ.includes("top"))
 
 /*export const TopsById =(id) => media.find(c => c.typ.top === top)
 export const BottomById =(id) => media.find(c => c.typ.bottom === bottom)
 export const ShoeById =(id) => media.find(c => c.typ.shoe === shoe)*/
 
-export const mediaByTyp = typ => media[typ % media.length].url;
+export const mediaByTyp = typ => mediaTop[typ % mediaTop.length].url;
 
 export default clothesJson
+
