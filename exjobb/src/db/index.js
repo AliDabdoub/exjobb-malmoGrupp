ab7d20967cfefd7e488a1055d0c073995943d7e6
tildel <tildelienau@gmail.com>
2022-10-19 14:45:21 +0200

    More carousels

--- exjobb/src/db/index.js
+++ exjobb/src/db/index.js
@@ -1,13 +1,13 @@
-import clothesJson from './clothes.json'
+import clothesJson from './sorted-clothes.json'
 
-export const media = clothesJson.clothes
-export const mediaTop = media.filter(typ => typ.includes("top"))
+export const media = clothesJson.clothes[2]
+
 
 /*export const TopsById =(id) => media.find(c => c.typ.top === top)
 export const BottomById =(id) => media.find(c => c.typ.bottom === bottom)
 export const ShoeById =(id) => media.find(c => c.typ.shoe === shoe)*/
 
-export const mediaByTyp = typ => mediaTop[typ % mediaTop.length].url;
+export const mediaByTyp = typ => media[typ % media.length].url;
 
 export default clothesJson
 
