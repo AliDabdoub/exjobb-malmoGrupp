56a11304686c2f122629b0c822d580450e18fb44
tildel <tildelienau@gmail.com>
2022-10-19 11:43:55 +0200

    Tilde Carousel

--- exjobb/src/db/index.js
+++ exjobb/src/db/index.js
@@ -3,7 +3,13 @@ import clothesJson from './clothes.json'
 export const media = clothesJson.clothes
 
 export const mediaById =(id) => media.find(c => c.id === id)
+//export const TopsById =(id) => media.find(c => c.id === id)
+//export const BottomsById =(id) => media.find(c => c.id === id)
+//export const ShoesById =(id) => media.find(c => c.id === id)
 
 export const mediaByIndex = index => media[index % media.length].url;
+//export const TopsByIndex = index => media[index % media.length].url;
+//export const BottomsByIndex = index => media[index % media.length].url;
+//export const ShoesByIndex = index => media[index % media.length].url;
 
 export default clothesJson
