ce3af5590fe36deb273b7af0eb24ef44c406dd6d
tildel <tildelienau@gmail.com>
2022-10-19 09:23:28 +0200

    Embla

--- exjobb/pages/create/index.js
+++ exjobb/pages/create/index.js
@@ -2,19 +2,7 @@ import React from "react";
 import EmblaCarousel from "components/EmblaCarousel/EmblaCarousel";
 // import db from "db";
 
-let demo = React.createElement( 
-    "h1", { style: { color: "green" } }, "Welcome to GeeksforGeeks"
-)
 
-
-
-function create (){
-    return(
-        <div>
-            <h1>create outfits page</h1>
-        </div>
-    )
-}
 
 const SLIDE_COUNT = 5;
 const slides = Array.from(Array(SLIDE_COUNT).keys());
