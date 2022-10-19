fb5d1933a872329870ffab6e7793e222d5a31820
SabrinaPawlikowski <112379041+SabrinaPawlikowski@users.noreply.github.com>
2022-10-19 11:45:18 +0200

    changed image bug

--- exjobb/src/db/index.js
+++ exjobb/src/db/index.js
@@ -2,7 +2,7 @@ import clothesJson from './clothes.json'
 
 export const media = clothesJson.clothes
 
-export const mediaById =(id) => media.find(c => c.id === id)
+export const mediaById =(id) => media.find(c => c.typ.top === top)
 
 export const mediaByIndex = index => media[index % media.length].url;
 
