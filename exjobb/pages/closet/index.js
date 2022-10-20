642a7e84675485ee24cf316e84409342b0f76458
alexbjorck <alexbjorck@gmail.com>
2022-10-20 14:42:12 +0200

    handleEvent index.js closet

--- exjobb/pages/closet/index.js
+++ exjobb/pages/closet/index.js
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react";
+import clothingId from "./[clothingId]" 
 // import "../clothes/clothes.json"
 
 export const getStaticProps = async () => {
-        const response = await fetch ('http://localhost:3000/api/closet')
-        const data = await response.json()
+    const response = await fetch('http://localhost:3000/api/closet')
+    const data = await response.json()
 
-        return{
-            props: { closet: data }
-        }
-} 
+    return {
+        props: { closet: data }
+    }
+}
 
 const Closet = ({ closet }) => {
-    return(
+
+    const handleClick = event => {
+        return(
+            
+        
+        )
+    }
+
+    return (
         <div>
-            {closet.clothes.map(c =>(
+            {closet.clothes.map(c => (
                 <div key={closet.id}>
-                    <img src={c.url}/>
+                    <img src={c.url}
+                        onClick={handleClick}
+                    />
                 </div>
             ))}
         </div>
