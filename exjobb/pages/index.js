4bf6f558f41001398f60003d8b425efa1e08f5ed
Ali Dabdoub <ali.dabdoub@outlook.com>
2022-10-21 15:07:35 +0200

    we made the navbar without styling

--- exjobb/pages/index.js
+++ exjobb/pages/index.js
@@ -1,8 +1,10 @@
 import styles from 'styles/Home.module.scss'
+import {Typography} from '@material-ui/core'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Typography variant="h1">test </Typography>
       <h1>home page</h1>
     </div>
   )
---
+++ exjobb/src/components/Navbar.js
@@ -0,0 +1,18 @@
+const { StylesContext } = require("@material-ui/styles")
+import styles from "styles/Navbar.module.scss";
+import Link from "next/link";
+
+const Navbar = ()=> {
+    return(
+        <nav className={styles.main__nav}>
+            <ul>
+                <Link href='/'><a><li>Home</li></a></Link>
+                <Link href='/closet'><a><li>Closet</li></a></Link>
+                <Link href='/create'><a><li>Create</li></a></Link>
+                <Link href='/outfit'><a><li>Outfit</li></a></Link>
+            </ul>
+        </nav>
+    )
+} 
+
+export default Navbar
