32cde85bc83f2618f8726ffda3ec90faa18251dc
Ali Dabdoub <ali.dabdoub@outlook.com>
2022-10-21 16:13:24 +0200

    just commit

--- exjobb/pages/index.js
+++ exjobb/pages/index.js
@@ -4,7 +4,6 @@ import {Typography} from '@material-ui/core'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Typography variant="h1">test </Typography>
       <h1>home page</h1>
     </div>
   )
