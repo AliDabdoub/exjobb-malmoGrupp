74f70ee68665ff14876db140196ace9df24c12c4
SabrinaPawlikowski <112379041+SabrinaPawlikowski@users.noreply.github.com>
2022-10-24 15:51:14 +0200

    home page styled

--- exjobb/pages/index.js
+++ exjobb/pages/index.js
@@ -1,9 +1,15 @@
 import styles from 'styles/Home.module.scss'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>home page</h1>
+      <div className={styles.linkUpper}><h1>Upload your <Link href='/closet'><a>closet</a></Link><br></br></h1></div>
+
+      <div className={styles.linkMittle}><h1><Link href='/create'><a>Create</a></Link> your outfits</h1></div>
+
+      <div className={styles.LinkBelow}><h1>Collet your <Link href='/outfit'><a>outfits</a></Link></h1></div>
+      
     </div>
   )
 }
