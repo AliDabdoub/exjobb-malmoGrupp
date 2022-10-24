86d3daebaca7cdabbf0aad6629a758139faa3238
tildel <tildelienau@gmail.com>
2022-10-24 09:54:18 +0200

    db index

--- exjobb/src/db/index.js
+++ exjobb/src/db/index.js
@@ -6,6 +6,8 @@ export const mediaByTop = media.filter(c => c.typ === 'top')
 export const mediaByBottom = media.filter(c => c.typ === 'bottom')
 export const mediaByShoe = media.filter(c => c.typ === 'shoe')
 
+export const getMediaById = (id) => media.find(c => Number(c.id) === Number(id))
+
 export const mediaByTyp = typ => media[typ % media.length].url;
 
 export default clothesJson
