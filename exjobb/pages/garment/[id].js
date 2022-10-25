77201c78bdc799b6e3e87dd02c332c99be3ef910
Ali Dabdoub <ali.dabdoub@outlook.com>
2022-10-25 13:34:30 +0200

    I added styling for garment page

--- exjobb/pages/garment/[id].js
+++ exjobb/pages/garment/[id].js
@@ -1,11 +1,14 @@
 import React from "react";
 import { getMediaById } from 'db'
+import Navbar from '../../src/components/Navbar'
+import styles from "styles/Garment.module.scss";
 
 const CreatePage = (props) => {
     const { garment = {} } = props
 
     return (
-        <div>
+        <div className={styles.garment}>
+            <Navbar/>
             <img src={garment.url} />
         </div>
     )
