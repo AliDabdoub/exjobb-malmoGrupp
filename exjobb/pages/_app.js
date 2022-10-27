d43b36d90c414e440a86b60db7b990813135497c
tildel <tildelienau@gmail.com>
2022-10-27 16:53:17 +0200

    Favicon

--- exjobb/pages/_app.js
+++ exjobb/pages/_app.js
@@ -1,11 +1,13 @@
 import 'styles/globals.css'
 import Navbar from '../src/components/Navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <>
-  <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
+    <Head>
+      <title>Capsule Collective</title>
+      <link rel="icon" href="/brandLogo.svg" />
+    </Head>
       
   <Component {...pageProps} />
   </>
