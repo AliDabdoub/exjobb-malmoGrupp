5db0d5167e981278acdddc556c8c70d9969f91d7
Ali Dabdoub <ali.dabdoub@outlook.com>
2022-10-28 11:09:54 +0200

    cleaning

--- exjobb/pages/api/closet/index.js
+++ exjobb/pages/api/closet/index.js
@@ -1,6 +1,7 @@
 import db from "db"
-//http://localhost:8000/clothes/2
 
 export default async function handler(req, res) {
     res.status(200).json(db || {})
 }
+
+//http://localhost:3000/api/clothes/2
--- exjobb/pages/api/clothes/[id].js
+++ exjobb/pages/api/clothes/[id].js
@@ -1,8 +1,8 @@
 import db from 'db'
-//http://localhost:8000/clothes/2
 
 export default async function handler(req, res) {
     const id = req.query.id 
     const cloth = db.clothes.find(c => c.id === Number(id))
     res.status(200).json(cloth || {})
 }
+//http://localhost:3000/api/clothes/1
